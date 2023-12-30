6f83b3628c8e20458078c7feddf5815f9b9d0644
EswarAbbireddy <eswarabbireddy9640@gmail.com>
2023-12-30 17:56:08 +0530

    30-12-2023completed

---
+++ test.js
@@ -0,0 +1,15 @@
+console.log("eswar");
+let a=[1,4,6,0,8,5,0,0,0]
+let b=[]
+
+for(var c=0;c<a.length;c++)
+{
+    if(a[c]!=0)
+    {
+        b[c]=a[c];
+    }
+}
+for(var d=0;d<b.length;d++)
+{
+    console.log(b[d])
+}
